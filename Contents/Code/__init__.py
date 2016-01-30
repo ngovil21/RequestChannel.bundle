749b6bb695128b545c168c27737198b59f343a21
ngovil21 <ngovil21@gmail.com>
2016-01-30 14:55:09 -0600

    change view group to grid in searches to see posters

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -80,7 +80,7 @@ def AddNewMovie(title, locked='unlocked'):
 
 @route(PREFIX + '/searchmovie')
 def SearchMovie(title, query, locked='unlocked'):
-    oc = ObjectContainer(title1=title,content=ContainerContent.Movies)
+    oc = ObjectContainer(title1=title,content=ContainerContent.Movies, view_group=ViewType.Grid)
     query = String.Quote(query, usePlus=True)
     if Prefs['movie_db'] == "TheMovieDatabase":
         headers = {
@@ -177,7 +177,7 @@ def AddNewTVShow(title="", locked='unlocked'):
 
 @route(PREFIX + '/searchtv')
 def SearchTV(query, locked='unlocked'):
-    oc = ObjectContainer(title1="Search Results", content=ContainerContent.Shows)
+    oc = ObjectContainer(title1="Search Results", content=ContainerContent.Shows, view_group=ViewType.Grid)
     query = String.Quote(query, usePlus=True)
     xml = XML.ElementFromURL(TVDB_API_URL + "GetSeries.php?seriesname=" + query)
     series = xml.xpath("//Series")
