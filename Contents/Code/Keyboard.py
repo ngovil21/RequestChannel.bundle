460dab0ff7fb3b225a3b7b44f4820193a64413f6
ngovil21 <ngovil21@gmail.com>
2016-02-09 00:28:04 -0600

    remove logging from Keyboard

--- Contents/Code/Keyboard.py
+++ Contents/Code/Keyboard.py
@@ -26,33 +26,33 @@ def Keyboard(query=None, callback=None, parent=None, shift=False, secure='False'
         message = None
     oc = ObjectContainer(title1=title, title2=string, message=message)
     # Submit
-    Log.Debug("Create Submit key")
+    # Log.Debug("Create Submit key")
     oc.add(DirectoryObject(key=Callback(callback, query=query, locked=locked), title=u'%s: %s' % ('Submit', string.replace(' ', '_'))))
     # Search History
     if Dict['DumbKeyboard-History']:
-        Log.Debug("Create History")
+        # Log.Debug("Create History")
         oc.add(DirectoryObject(key=Callback(History, query=query, callback=callback, locked=locked, secure=secure), title=u'%s' % 'Search History'))
     # Space
-    Log.Debug("Create Space Key")
+    # Log.Debug("Create Space Key")
     oc.add(DirectoryObject(key=Callback(Keyboard, query=query + " " if query else " ", callback=callback,parent=parent, locked=locked, secure=secure, title=title),
                            title='Space'))
     # Backspace (not really needed since you can just hit back)
     if query is not None:
-        Log.Debug("Create backspace key")
+        # Log.Debug("Create backspace key")
         oc.add(DirectoryObject(key=Callback(Keyboard, query=query[:-1], callback=callback, parent=parent, locked=locked, secure=secure, title=title), title='Backspace'))
     # Shift
-    Log.Debug("Create Shift Key")
+    # Log.Debug("Create Shift Key")
     oc.add(
         DirectoryObject(key=Callback(Keyboard, query=query, callback=callback, parent=parent, locked=locked, secure=secure, shift=True, title=title), title='Shift'))
     # Keys
-    Log.Debug("Generating keys")
+    # Log.Debug("Generating keys")
     if parent:
         oc.add(DirectoryObject(key=Callback(parent, locked=locked), title="Cancel"))
     for key in KEYS if not shift else SHIFT_KEYS:
         oc.add(
             DirectoryObject(key=Callback(Keyboard, query=query + key if query else key, callback=callback, parent=parent, locked=locked, secure=secure, title=title),
                             title=u'%s' % key))
-    Log.Debug("Return Object Container")
+    # Log.Debug("Return Object Container")
     return oc
 
 
