aeb58cb02ca43e9c58a816abc2fc633c61aeab0c
ngovil21 <ngovil21@gmail.com>
2016-02-10 02:03:07 -0600

    make episode a list

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -944,8 +944,8 @@ def SonarrMonitorShow(series_id, seasons, episodes='all', locked='unlocked'):
             for e in episode_list:
                 episode = JSON.ObjectFromURL(sonarr_url + "/api/Episode/" + str(e), headers=api_header)
                 episode['monitored'] = True
-                data = JSON.StringFromObject(episode)
-                HTTP.Request(sonarr_url + "/api/Episode/", headers=api_header, data=data)
+                data = JSON.StringFromObject(list(episode))
+                HTTP.Request(sonarr_url + "/api/Episode", headers=api_header, data=data)
             data2 = JSON.StringFromObject({'episodeIds': episodes})
             HTTP.Request(sonarr_url + "/api/command/EpisodeSearch/", headers=api_header, data=data2)
             # except Exception as e:
