e48c42a6bf7b738c32c4e717b0f3a428d23d0c61
ngovil21 <ngovil21@gmail.com>
2016-02-21 11:15:20 -0600

    fix for checkAdmin

--- Contents/Code/Session.py
+++ Contents/Code/Session.py
@@ -1501,7 +1501,7 @@ class Session:
 def checkAdmin(toke):
     import urllib2
     try:
-        url = "https://plex.tv/users/account" if Prefs['plextv'] else "http://127.0.0.1:32400/"
+        url = "https://plex.tv/users/account" if Prefs['plextv'] else "http://127.0.0.1:32400/myplex/account"
         req = urllib2.Request(url, headers={'X-Plex-Token': toke})
         resp = urllib2.urlopen(req)
         if resp.read():
