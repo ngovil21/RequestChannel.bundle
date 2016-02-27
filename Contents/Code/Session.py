576d3e725fd28a14c724b4e6217d6d20e825897f
ngovil21 <ngovil21@gmail.com>
2016-02-27 11:24:01 -0600

    fix message for reportproblemmediaother

--- Contents/Code/Session.py
+++ Contents/Code/Session.py
@@ -1612,9 +1612,9 @@ class Session:
 
     def ReportProblemMediaOther(self, query="", report=""):
         if not query:
-            oc = ObjectContainer()
+            oc = ObjectContainer(title2="Report Problem with Media")
             if Client.Platform == "Plex Web":
-                oc.message = "Enter your problem in the search box."
+                oc = ObjectContainer(header=TITLE, message = "Enter your problem in the search box.")
             oc.add(
                 InputDirectoryObject(key=Callback(self.ReportProblemMediaOther, report=report), title="Other Problem",
                                      prompt="What is the problem?"))
