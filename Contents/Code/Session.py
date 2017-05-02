5d4eadf2d9ec48d9d9d30c32f8c8ede2bb03a2f5
ngovil21 <ngovil21@gmail.com>
2017-05-02 14:12:53 -0500

    DEVELOPMENT Branch

--- Contents/Code/Session.py
+++ Contents/Code/Session.py
@@ -17,7 +17,7 @@ ART = 'art-default.jpg'
 ICON = 'plexrequestchannel.png'
 
 VERSION = "0.9.1"
-BRANCH = "TEST"
+BRANCH = "DEVELOPMENT"
 CHANGELOG_URL = "https://raw.githubusercontent.com/ngovil21/RequestChannel.bundle/" + BRANCH + "/CHANGELOG"
 
 ### URL Constants for TheMovieDataBase ##################
