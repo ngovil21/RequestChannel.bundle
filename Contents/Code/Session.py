a1d18355c6780b8aa35200b812ead482fb46aeb8
ngovil21 <ngovil21@gmail.com>
2017-05-02 12:34:57 -0500

    do not use handler for route

--- Contents/Code/Session.py
+++ Contents/Code/Session.py
@@ -11,7 +11,7 @@ from api import *
 import Helper
 
 TITLE = 'Request Channel'
-PREFIX = '/video/requestchannel'
+PREFIX = '/requestchannel'
 
 ART = 'art-default.jpg'
 ICON = 'plexrequestchannel.png'
